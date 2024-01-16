09a9967733e6c7b2d705ac98bfc128cd27da963b
JethroNatividad <natividad.jet@gmail.com>
2024-01-16 12:50:24 +0800

    write program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+// Write a program that determines a passwords strength
+// • A very weak password contains only numbers and is
+// fewer than eight characters.
+// • A weak password contains only letters and is fewerthan
+// eight characters.
+// • A strong password contains letters and at least one
+// number and is at least eight characters.
+// • A very strong password contains letters, numbers, and
+// special characters and is at least eight characters.
+
 fn main() {
     println!("Hello, world!");
 }
