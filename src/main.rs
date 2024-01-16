3f5489edde6d169928baec656723c9782ed289d9
JethroNatividad <natividad.jet@gmail.com>
2024-01-16 13:14:37 +0800

    write tests

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,27 @@
 // Process: checks password strength
 // Outputs: The password '{}' is a {very weak | weak | strong | very strong} password.
 
+// The password '12345' is a very weak password.
+// The password 'abcdef' is a weak password.
+// The password 'abc123xyz' is a strong password.
+// The password '1337h@xor!' is a very strong password.
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_password_strength() {
+        assert_eq!(get_password_strength("12345"), PasswordStrength::VeryWeak);
+        assert_eq!(get_password_strength("abcdef"), PasswordStrength::Weak);
+        assert_eq!(get_password_strength("abc123xyz"), PasswordStrength::Strong);
+        assert_eq!(
+            get_password_strength("1337h@xor!"),
+            PasswordStrength::VeryStrong
+        );
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
